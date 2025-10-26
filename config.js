9f0605a69f58ddff2e26fd28695b20af378f1cd0
carigothaya42-wq <carigothaya42@gmail.com>
2025-10-26 12:52:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID, 
+SESSION_ID: process.env.SESSION_ID === undefined ? 'DBkyTChD#jGlxCAU73MhCiQu8sZGJU6-9AuabyCWk8DASYjsXsoY' : process.env.SESSION_ID, 
 PORT: process.env.PORT === undefined ? '8000' : process.env.PORT,
 SESSION_NAME: process.env.SESSION_NAME === undefined ? 'nadeenmd' : process.env.SESSION_NAME
 };
